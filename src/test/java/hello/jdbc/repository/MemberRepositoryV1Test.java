b134e06e9dcbae54f31c1bed0349ed1546ed2698
eskim <eunsunkim13@gmail.com>
2022-06-12 15:05:09 +0900

    TEST: Add Transaction 테스트

--- src/test/java/hello/jdbc/repository/MemberRepositoryV1Test.java
+++ src/test/java/hello/jdbc/repository/MemberRepositoryV1Test.java
@@ -18,12 +18,7 @@ import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
 @Slf4j
 class MemberRepositoryV1Test {
-    public static final String MEMBER_A = "memberA";
-    public static final String MEMBER_B = "memberB";
-    public static final String MEMBER_EX = "ex";
-
     private MemberRepositoryV1 memberRepository;
-    private MemberServiceV1 memberService;
 
     @BeforeEach
     void beforeEach() throws Exception {
@@ -61,54 +56,5 @@ class MemberRepositoryV1Test {
         memberRepository.delete(member.getMemberId());
         assertThatThrownBy(() -> memberRepository.findById(member.getMemberId()))
                 .isInstanceOf(NoSuchElementException.class);
-    }
-
-    @AfterEach
-    void after() throws SQLException {
-        memberRepository.delete(MEMBER_A);
-        memberRepository.delete(MEMBER_B);
-        memberRepository.delete(MEMBER_EX);
-    }
-
-    @Test
-    @DisplayName("정상 이체")
-    void accountTransfer() throws SQLException {
-        // given
-        Member memberA = new Member(MEMBER_A, 10000);
-        Member memberB = new Member(MEMBER_B, 10000);
-        memberRepository.save(memberA);
-        memberRepository.save(memberB);
-
-        //when
-        memberService.accountTransfer(memberA.getMemberId(), memberB.getMemberId(), 2000);
-
-        // then
-        Member findMemberA = memberRepository.findById(memberA.getMemberId());
-        Member findMemberB = memberRepository.findById(memberB.getMemberId());
-
-        assertThat(findMemberA.getMoney()).isEqualTo(8000);
-        assertThat(findMemberB.getMoney()).isEqualTo(12000);
-    }
-
-    @Test
-    @DisplayName("이체 중 예외 발생")
-    void accountTransferEx() throws SQLException {
-        // given
-        Member memberA = new Member(MEMBER_A, 10000);
-        Member memberEx = new Member(MEMBER_EX, 10000);
-        memberRepository.save(memberA);
-        memberRepository.save(memberEx);
-
-        // when
-        assertThatThrownBy(() -> memberService.accountTransfer(memberA.getMemberId(), memberEx.getMemberId(), 2000))
-                .isInstanceOf(IllegalStateException.class);
-
-        // then
-        Member findMemberA = memberRepository.findById(memberA.getMemberId());
-        Member findMemberEx = memberRepository.findById(memberEx.getMemberId());
-
-        // memberA의 돈만 2000원 줄었고, ex의 돈은 10000원 그대로이다.
-        assertThat(findMemberA.getMoney()).isEqualTo(8000);
-        assertThat(findMemberEx.getMoney()).isEqualTo(10000);
     }
 }
